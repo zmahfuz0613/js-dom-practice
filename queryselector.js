62a1301ba32543fcd4cac2aee5421b85cf5331fe
superbuggy <jpqreichard@gmail.com>
2018-03-22 12:12:08 -0400

    Add 2 querySelector exercises

--- queryselector.js
+++ queryselector.js
@@ -1,16 +1,10 @@
-/*
- *  Get the element with an ID of 'first' using querySelector:
- */
 
-/*
- * Get the elements with a class of 'second' with querySelector:
- */
+// Get the element with an ID of 'first' using querySelector...
 
-/*
- * Try the above prompt with querySelectorAll. What's the difference
- * between what these two methods return?
- */
+// Get the elements with a class of 'second' with querySelector...
 
-/*
- * Get the span element using querySelector:
- */
+// Try the above prompt with querySelectorAll. What's the difference between what these two methods return?
+
+// Get the span element using querySelector...
+
+// Select only any link tag(s) *directly inside* of a div (no grandchildren).
