3b32b6c09166111f3a507527801b03c3d6f17996
ZUNAYED MAHFUZ <str053mgr@gmail.com>
2020-07-07 13:54:51 -0400

    Update getelements.js

--- getelements.js
+++ getelements.js
@@ -2,13 +2,17 @@
 
 
 /* Get the element with an ID of 'first' */
+const firstElement = document.getElementById("first")
+console.log(firstElement)
 
 /* Now delete that first element (in the html file), and re-run your code. What changed? */
 
 /* Get the elements with a class of 'second' with getElementsByClassName: */
-
+const second = document.getElementsByClassName('second')
+console.log(second)
 /* Delete one of the elements with a class of 'second' and refresh the page. Do you get a similar result? */
  
 /* Get the span element using getElementsByTagname: */
-
+const span = document.getElementsByTagName('span')
+console.log(span)
 /* Add a span anywhere, and re-run the last piece of code. What's different? */
