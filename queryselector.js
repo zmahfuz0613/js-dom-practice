86099d596a5e154f6dd191bc2978decb51e94020
Zunayed Mahfuz <zunayedmahfuz@Zunayeds-MacBook-Air.local>
2020-07-07 15:02:12 -0400

    Updated solution Lab-2

--- queryselector.js
+++ queryselector.js
@@ -4,10 +4,10 @@ const firstElement = document.getElementById("first")
 console.log(firstElement)
 
 // Get the elements with a class of 'second' with querySelector
-const second = document.getElementsByClassName('second')
+const second = document.querySelector('.second')
 console.log(second)
 // Try the above prompt with querySelectorAll. What's the difference between what these two methods return?
-const prompt = document.querySelectorAll('elements')
+const prompt = document.querySelectorAll('.second')
 console.log(prompt)
 // Get the span element using querySelector
 const span = document.querySelector('span')
@@ -16,12 +16,12 @@ console.log(span)
 const multipleElements = document.querySelectorAll('span')
 console.log(multipleElements)
 // Select only "a" tags *directly inside* of a div (no grandchildren).
-const paras = document.getElementsByTagName('a')
+const paras = document.querySelector('div >a')
 console.log(paras)
 // Select all elements that contain a "data-target" attribute
-const dataTarget = document.querySelectorAll('li')
+const dataTarget = document.querySelectorAll('[data-target]')
 console.log(dataTarget)
 // Select all elements where the data-target attribute equals "#false"
 
-const para = document.querySelectorAll('li')
+const para = document.querySelectorAll('[data-target="#false"]')
 console.log(para)
